343d6ea36da5e0f7a58075320724cd73a4ac8dce
PlantChromatin <turck@mpipz.mpg.de>
2023-12-08 15:33:54 +0100

    Update EPIC_ChIPseq_1.get.reads.bash

--- scripts/EPIC_ChIPseq_1.get.reads.bash
+++ scripts/EPIC_ChIPseq_1.get.reads.bash
@@ -10,13 +10,18 @@ mkdir -p ${path_reads}
 mkdir -p ${path_reference}
 mkdir -p ${path_annotation}
 
-echo "downloading the reference files from the European Nucleotide Archive"
+echo "downloading the read files from the European Nucleotide Archive"
 
 file="$1"
+#which column has the header "fastq_ftp"?
+link=$(awk -F ',' 'NR==1{for(i=1;i<=NF;i++){if($i=="fastq_ftp"){col=i;break}}}{print $col}' "${working_directory}/${file}")
+#now download the list of files:
 while read line; do
-adress=$(echo $line | cut -d ' ' -f 10)
+adress=$(echo $line | cut -d '\t' -f ${link})
 wget -P ${path_reads} $adress
-done < "${working_directory}/${file}"
+done <(tail -n +2 "${working_directory}/${file}")
+
+echo "done downloading the read files from the European Nucleotide Archive"
 
 echo "downloading the Arabidopsis thaliana reference genome"
 
@@ -29,6 +34,7 @@ awk 'BEGIN {FS="\t"}; {print $1 FS $2}' ${path_reference}/TAIR10_chr_all.fas.fai
 awk 'BEGIN {FS="\t"}; ($1 ~ /Chr[1-5]/) {print $1 FS "0" FS $2}' ${path_reference}/TAIR10_chr_all.fas.fai > ${path_reference}/TAIR10.chr_nuclear.bed
 awk 'BEGIN {FS="\t"}; ($1 ~ /Chr[1-5]/) {print $1 FS $2}' ${path_reference}/TAIR10_chr_all.fas.fai > ${path_reference}/TAIR10.chrlength_nuclear.txt
 
+echo "done downloading the Arabidopsis thaliana reference genome"
 
 echo "downloading the Arabidopsis genome annotation file Araport11"
 
@@ -36,3 +42,5 @@ wget -P ${path_annotation} https://www.arabidopsis.org/download_files/Genes/Arap
 gzip -d  ${path_annotation}/Araport11_GFF3_genes_transposons.current.gff.gz
 wget -P ${path_annotation} https://www.arabidopsis.org/download_files/Genes/Araport11_genome_release/Araport11_GTF_genes_transposons.current.gtf.gz
 gzip -d  ${path_annotation}/Araport11_GTF_genes_transposons.current.gtf.gz
+
+echo "done downloading the Arabidopsis genome annotation file Araport11"
